688ee4f92caf1e4a592b304d5364a03a686e6fea
kharvadarshan <150101961+kharvadarshan@users.noreply.github.com>
2025-07-25 00:42:47 +0530

    Added Item ID field to To-Do form

--- ToDoPage.js
+++ ToDoPage.js
@@ -2,29 +2,15 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function TodoPage() {
-  const [itemName, setItemName] = useState('');
-  const [itemDescription, setItemDescription] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await axios.post('/submittodoitem', { itemName, itemDescription });
-    alert('Item submitted');
-  };
+    const [itemId, setItemId] = useState('');
 
   return (
     <form onSubmit={handleSubmit}>
       <input
-        type="text"
-        placeholder="Item Name"
-        value={itemName}
-        onChange={(e) => setItemName(e.target.value)}
-        required
-      />
-      <textarea
-        placeholder="Item Description"
-        value={itemDescription}
-        onChange={(e) => setItemDescription(e.target.value)}
-        required
+      type="number"
+      placeholder="Item ID"
+      value={itemId}
+      onChange={(e) => setItemId(e.target.value)}
       />
       <button type="submit">Add To-Do</button>
     </form>
